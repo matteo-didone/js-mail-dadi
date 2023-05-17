067bbe8ee40c7d2dc4eea209f2e23662c36dc28e
matteo-didone <matteo.dido9@gmail.com>
2023-05-17 15:35:15 +0200

    Now, I added the p elements to the index file

--- script.js
+++ script.js
@@ -14,32 +14,43 @@ function generateRandomNumber()
 const userNumber = generateRandomNumber();
 const computerNumber = generateRandomNumber();
 
+//Create a p to add the results to the DOM
+let userScoreP = document.getElementById('userScore');
+let computerScoreP = document.getElementById('computerScore'); 
+let scoreComparisonP = document.getElementById('scoreComparison');
+
+
 //Compare the 2 numbers
 function compareNumbers(userNumber, computerNumber) 
 {
+    //Display the 2 numbers in the console
     console.log("User number: " + userNumber);
     console.log("Computer number: " + computerNumber);
+
+    //Display the 2 numbers in the DOM
+    userScoreP.append("User number: " + userNumber);
+    computerScoreP.append("Computer number: " + computerNumber);
 
     if(userNumber > computerNumber) 
     {
         const userScore = "You win!";
         console.log(userScore);
+        scoreComparisonP.append(userScore);
     }
 
     else if(userNumber < computerNumber) 
     {
         const computerScore = "You lose!";
         console.log(computerScore);
+        scoreComparisonP.append(computerScore);
     }
     else if (userNumber === computerNumber)
     {
         const tieScore = "It's a tie!";
-        console.log(tie);
+        console.log(tieScore);
+        scoreComparisonP.append(tieScore);
     }
 }
 
+//Display the results
 compareNumbers(userNumber, computerNumber);
-
-
-
-//Display the final result
